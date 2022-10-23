c0dc9a5011ac075b8848cc716bdcd7cf97449c7d
guest271314 <guest271314@gmail.com>
2022-10-22 22:04:20 -0700

    Use strlen(), include messageLength in comments

--- nm_c.c
+++ nm_c.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 // Parameter `inputLength`: a pointer to a single `size_t` variable. 
 // `size_t` is an unsigned integer type big enough to represent the length of any 
 // memory sequence, it's usually 8 bytes.
@@ -20,7 +19,7 @@ uint8_t* getMessage(size_t *inputLength) {
   uint32_t messageLength = 0;
   // The operator `&` gives a pointer to the variable, so `fread()` writes directly
   // into `inputLength`. 
-  fread(&messageLength, sizeof(messageLength), 1, stdin);
+  size_t result = fread(&messageLength, sizeof(messageLength), 1, stdin);
   // Allocate a buffer that is 'messageLength' elements long. `calloc() and 
   // `malloc()` return a pointer to memory on the heap.
   // The operator `*` dereferences a pointer (`*message' is equivalent to 
@@ -28,14 +27,15 @@ uint8_t* getMessage(size_t *inputLength) {
   // `sizeof(*message)` will hence return the size in bytes of the type 
   // `message` points at, which means it's equivalent to `sizeof(uint_8)`.
   uint8_t *message = calloc(messageLength, sizeof(*message));
-  fread(message, sizeof(*message), messageLength, stdin);
+  result = fread(message, sizeof(*message), messageLength, stdin);
   // `inputLength` is a pointer, so we store the length at the memory address it 
   // points at. This way we return 2 values at once from a function!
   *inputLength = messageLength;
   return message;
 }
 
-void sendMessage(uint8_t *response, int32_t responseLength) {
+void sendMessage(uint8_t *response /*, int32_t responseLength */) {
+  const uint32_t responseLength = strlen(response);
   fwrite(&responseLength, sizeof responseLength, 1, stdout);
   fwrite(response, responseLength, 1, stdout);
   fflush(stdout);
@@ -52,7 +52,7 @@ int main(void) {
     // example:
     // message[5] = 'a'; // Ok, pointer still points to start of message.
     // message = NULL; // Bad! Now we can't `free()` the message.
-    sendMessage(message, messageLength);
+    sendMessage(message /*, messageLength */);
     free(message);
   }
 }
